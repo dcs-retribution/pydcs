e466df086eac94ff7bad155adbbf39b0fd84cb25
Bobby Moretti <bobmoretti@gmail.com>
2022-12-22 21:04:00 -0800

    Add method for creating vehicle groups by list of Vehicle objects

--- dcs/country.py
+++ dcs/country.py
@@ -38,7 +38,7 @@ class Country:
         self.callsign_numbers: Dict[str, Set[int]] = {}
         self._tail_numbers: Set[str] = set()
 
-    def add_vehicle_group(self, vgroup):
+    def add_vehicle_group(self, vgroup) -> None:
         self.vehicle_group.append(vgroup)
 
     def add_ship_group(self, sgroup):
