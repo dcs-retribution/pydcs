60fa0d1cccf5fbd859de41da535266ac6a30cb12
Raffson <Raffson@users.noreply.github.com>
2023-03-04 14:00:15 +0100

    Gracefully handle JSON decode errors
    Partial fix dcs-liberation/dcs_liberation#2747

--- dcs/liveries_scanner.py
+++ dcs/liveries_scanner.py
@@ -31,12 +31,15 @@ def read_retribution_preferences() -> Tuple[str, str]:
     pref_path = os.path.join(os.path.expanduser("~"), "AppData", "Local", "DCSRetribution")
     pref_path = os.path.join(pref_path, "retribution_preferences.json")
     if os.path.exists(pref_path):
-        with open(pref_path, "r") as file:
-            json_dict = json.load(file)
-            if "dcs_install_dir" in json_dict:
-                install = json_dict["dcs_install_dir"]
-            if "saved_game_dir" in json_dict:
-                saved_games = json_dict["saved_game_dir"]
+        try:
+            with open(pref_path, "r") as file:
+                json_dict = json.load(file)
+                if "dcs_install_dir" in json_dict:
+                    install = json_dict["dcs_install_dir"]
+                if "saved_game_dir" in json_dict:
+                    saved_games = json_dict["saved_game_dir"]
+        except json.JSONDecodeError as e:
+            logging.exception(e.msg)
     return install, saved_games
 
 
