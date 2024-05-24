98299e2ef1cf1a29303dc8b4d2a1dd9844af2f0f
Raffson <Raffson@users.noreply.github.com>
2024-05-24 18:18:08 +0200

    Add F-4E-45MC to flyables map

--- tools/pydcs_export.lua
+++ tools/pydcs_export.lua
@@ -232,6 +232,7 @@ flyable["F-14B"] = true
 flyable["F-15C"] = true
 flyable["F-16C_50"] = true
 flyable["FA-18C_hornet"] = true
+flyable["F-4E-45MC"] = true
 flyable["F-5E-3"] = true
 flyable["F-86F Sabre"] = true
 flyable["Hawk"] = true
