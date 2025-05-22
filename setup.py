41e74e42b1e25a1010dcadc48676f4be5e6622f7
Cedric Leblond-Menard <cedlmen@hotmail.com>
2025-05-22 10:42:50 -0400

    Missing comma -.-

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'dcs/terrain/afghanistan',
         'dcs/terrain/caucasus',
         'dcs/terrain/falklands',
-        'dcs/terrain/germanycoldwar'
+        'dcs/terrain/germanycoldwar',
         'dcs/terrain/iraq',
         'dcs/terrain/kola',
         'dcs/terrain/marianaislands',
