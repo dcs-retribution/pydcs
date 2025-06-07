fda2a5319673931fc8995db4b1776c88e01a61be
Raffson <Raffson@users.noreply.github.com>
2025-06-07 18:21:36 +0200

    Update Germany Cold War map center x/y coords

--- dcs/terrain/germanycoldwar/germanycoldwar.py
+++ dcs/terrain/germanycoldwar/germanycoldwar.py
@@ -29,7 +29,7 @@ class GermanyColdWar(Terrain):
             PARAMETERS,
             # bounds from approximate bounds in MissionEditor (placing units at borders)
             bounds=mapping.Rectangle(-425000.0, -600000.0, -1100000.0, 260000.0, self),
-            map_view_default=MapView(mapping.Point(0, 0, self), self, 1000000),
+            map_view_default=MapView(mapping.Point(-170000, -762500, self), self, 1000000),
         )
         self.bullseye_blue = {"x": -384, "y": -781}
         self.bullseye_red = {"x": -384, "y": -781}
